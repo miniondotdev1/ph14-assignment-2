3b1d0c65f667865662c3f47086051d19dca93287
my first github repo <mrarifmondol98@gmail.com>
2026-07-31 12:16:34 +0600

    JS assingment problem solving

--- problem-1.js
+++ problem-1.js
@@ -1,5 +1,18 @@
 function matchWinner(teamAGoals, teamBGoals) {
-  // Write your code here…
+  if(typeof teamAGoals !== "number" || typeof teamBGoals !== "number"){
+    return "Invalid"
+  }
+  else if(teamAGoals > teamBGoals){
+    return "Team A Won";
+  }
+  else if(teamAGoals < teamBGoals){
+    return "Team B Won";
+  }
+  else return "Draw";
 }
 
-
+console.log(matchWinner(2,1))
+console.log(matchWinner(1,3))
+console.log(matchWinner(2,2))
+console.log(matchWinner("3",2))
+console.log(matchWinner(3, null))
---
+++ problem-2.js
@@ -0,0 +1,4 @@
+function isElevatorSafe(weights) {
+  // Write your code here
+}
+
