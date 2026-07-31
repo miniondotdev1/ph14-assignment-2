f6004aba6753321d9137bcfc924c06e7953dce6d
my first github repo <mrarifmondol98@gmail.com>
2026-07-31 12:29:36 +0600

    JS Assingment problem solving

--- problem-2.js
+++ problem-2.js
@@ -1,4 +1,25 @@
 function isElevatorSafe(weights) {
-  // Write your code here
+    let sum = 0;
+    for(let i =0; i< weights.length; i++){
+        let weight = weights[i]
+        sum += weight;
+    }
+    
+    if(!Array.isArray(weights)){
+        return "Invalid"
+    }
+    else if (sum <= 400){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
+console.log(isElevatorSafe([60,75,50]))
+console.log(isElevatorSafe([90,100,95,120]))
+console.log(isElevatorSafe([400]))
+console.log(isElevatorSafe("60,75,50"))
+console.log(isElevatorSafe([]))
+console.log(isElevatorSafe({"weight":60}))
 
